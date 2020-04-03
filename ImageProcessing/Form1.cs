b8ee5b65d34b12f74f9040a74a72dd764babd643
Abe Tomohiro <t.abe.solacom@gmail.com>
2020-04-03 11:07:15 +0900

    最初のコミット

---
+++ ImageProcessing/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 画像の読み込み
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko.jpg"))
+            {
+                // 画像をウィンドウに表示
+                Cv2.ImShow("sample_show", mat);
+            }
+        }
+    }
+}
