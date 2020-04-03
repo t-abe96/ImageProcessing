8cc24f4e8e9ac81c025c5139aa4def8ab56741a3
Abe Tomohiro <t.abe.solacom@gmail.com>
2020-04-03 13:20:53 +0900

    [add]テンプレートマッチ2の機能を追加

--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -89,7 +89,7 @@ namespace ImageProcessing
                 var thereshold = 0.9;
                 if (maxval >= thereshold)
                 {
-                    // 最も見るかった場所に赤枠を表示
+                    // 最も見つかった場所に赤枠を表示
                     Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
                     Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
 
@@ -103,7 +103,50 @@ namespace ImageProcessing
                 }
             }
         }
+        /* 
+         * 
+         * テンプレートマッチング2
+         * 
+         */
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 検索対象の画像とテンプレート画像
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko2.jpg"))
+            using (Mat temp = new Mat(@"D:\cs_source\img\template1.jpg"))
+            using (Mat result = new Mat())
+            {
+                // テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
 
+                // 閾値の範囲に絞る
+                Cv2.Threshold(result, result, 0.8, 1.0, ThresholdTypes.Tozero);
 
+                while (true)
+                {
+                    // 類似度が最大/最少となる画素の位置を調べる
+                    OpenCvSharp.Point minloc, maxloc;
+                    double minval, maxval;
+                    Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                    var threshold = 0.8;
+                    if (maxval >= threshold)
+                    {
+                        // 見つかった場所に赤枠を表示
+                        Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                        Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                        // 見つかった個所は塗りつぶす
+                        Rect outRect;
+                        Cv2.FloodFill(result, maxloc, new OpenCvSharp.Scalar(0), out outRect, new OpenCvSharp.Scalar(0.1),
+                            new OpenCvSharp.Scalar(1.0), FloodFillFlags.Link4);
+                    } else
+                    {
+                        break;
+                    }
+                }
+                // ウィンドウに画像を表示
+                Cv2.ImShow("temlate2_show", mat);
+            }
+        }
     }
 }
